c714fe630aad4c7aff385a534c874fe425a5d85e
Kennymorgan <ezeugwukenneth42@gmail.com>
2018-11-15 22:40:44 +0100

    updated ui

--- api/test.js
+++ api/test.js
@@ -1,6 +1,6 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import app from '../app';
+import app from './app';
 
 chai.use(chaiHttp);
 let should = chai.should();
@@ -61,5 +61,17 @@ describe('GET/:parcelid parcel', () => {
         });
     });
 
-
+    describe('Users', ()=> {
+        describe('GET/ users', () => {
+            it('it should GET all the parcels sent by a given user', (done) => {
+                chai.request(app)
+                .get('/api/v1/users/:userId/parcels')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                done();
+                });
+            });
+        });
+    });
 });
