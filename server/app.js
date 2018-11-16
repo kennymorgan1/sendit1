dc654bc0885dec0d5f5569a029edca20f3e5ca02
Kennymorgan <ezeugwukenneth42@gmail.com>
2018-11-16 12:40:16 +0100

    set up code coverage

--- server/app.js
+++ server/app.js
@@ -14,20 +14,6 @@ app.use(bodyParser.json());
 app.use('/api/v1/parcels', parcelRoutes);
 app.use('/api/v1/users', userRoutes);
 
-app.use((req, res, next) => {
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);    
-});
-
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
-});
 
 
 const port = process.env.PORT || 3000;
