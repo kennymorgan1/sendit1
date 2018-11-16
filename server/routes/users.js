a37d8dd30b669dc3f8e511fa6ed8aa91669e508b
Kennymorgan <ezeugwukenneth42@gmail.com>
2018-11-16 11:09:33 +0100

    fix ui

--- server/routes/users.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import UsersController from '../controllers/usersControllers';
-
-
-const router = express.Router();
-
-router.get('/:userId/parcels', UsersController.usersGetAll);
-
-export default router;
